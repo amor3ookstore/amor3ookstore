bac3a239f3ec7fcc6acf83b47eda5e46ca2814a0
amor3ookstore <amor3ookstore@gmail.com>
2025-06-29 13:22:58 +0200

    Create OrderItem.java

---
+++ src/main/java/com/amorbookstore/entity/OrderItem.java
@@ -0,0 +1,31 @@
+package com.amorbookstore.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "order_items")
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    private int quantity;
+
+    private BigDecimal price;
+}
