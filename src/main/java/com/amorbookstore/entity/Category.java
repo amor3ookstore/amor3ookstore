813072483f12579f5283489e3ea7a870a855dc18
amor3ookstore <amor3ookstore@gmail.com>
2025-06-29 13:17:40 +0200

    Create Category.java

---
+++ src/main/java/com/amorbookstore/entity/Category.java
@@ -0,0 +1,25 @@
+package com.amorbookstore.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Book> books;
+}
