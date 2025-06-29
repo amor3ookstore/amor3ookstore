c9136488a119ad03910fc1a495e2e31913fb1fff
amor3ookstore <amor3ookstore@gmail.com>
2025-06-29 13:28:06 +0200

    Create CategoryRepository.java

---
+++ src/main/java/com/amorbookstore/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.amorbookstore.repository;
+
+import com.amorbookstore.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    boolean existsByName(String name);
+}
