92a21e3846da396c9bdf6e08eb21c7c02ec30ad0
amor3ookstore <amor3ookstore@gmail.com>
2025-06-29 13:26:20 +0200

    Create RegisterRequest.java

---
+++ src/main/java/com/amorbookstore/dto/RegisterRequest.java
@@ -0,0 +1,25 @@
+package com.amorbookstore.dto;
+
+import lombok.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RegisterRequest {
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 6)
+    private String password;
+}
