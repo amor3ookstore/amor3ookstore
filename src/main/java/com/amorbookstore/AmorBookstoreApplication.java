7577b101daa23ee800e585779964022416535905
amor3ookstore <amor3ookstore@gmail.com>
2025-06-29 13:45:25 +0200

    Create AmorBookstoreApplication.java

---
+++ src/main/java/com/amorbookstore/AmorBookstoreApplication.java
@@ -0,0 +1,11 @@
+package com.amorbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmorBookstoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AmorBookstoreApplication.class, args);
+    }
+}
