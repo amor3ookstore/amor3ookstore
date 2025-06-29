c24ad30009a7541f9e8e844d1f7ff87059ee58ba
amor3ookstore <amor3ookstore@gmail.com>
2025-06-29 13:30:03 +0200

    Create CategoryService.java

---
+++ src/main/java/com/amorbookstore/service/CategoryService.java
@@ -0,0 +1,13 @@
+package com.amorbookstore.service;
+
+import com.amorbookstore.dto.CategoryDTO;
+
+import java.util.List;
+
+public interface CategoryService {
+    List<CategoryDTO> getAllCategories();
+    CategoryDTO getCategoryById(Long id);
+    CategoryDTO createCategory(CategoryDTO categoryDTO);
+    CategoryDTO updateCategory(Long id, CategoryDTO categoryDTO);
+    void deleteCategory(Long id);
+}
