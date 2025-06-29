51cf94afc709f09efd766b4735df0c55f1f81a22
amor3ookstore <amor3ookstore@gmail.com>
2025-06-29 13:24:07 +0200

    Create UserDTO.java

---
+++ src/main/java/com/amorbookstore/dto/UserDTO.java
@@ -0,0 +1,15 @@
+package com.amorbookstore.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserDTO {
+    private Long id;
+    private String username;
+    private String email;
+    private boolean enabled;
+}
