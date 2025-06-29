9f37f66942743ae7f19a1cbf3fb6083d8b55472e
amor3ookstore <amor3ookstore@gmail.com>
2025-06-29 13:13:47 +0200

    Create Amor3ookstoreApplication.java

---
+++ src/main/java/com/amorbookstore/Amor3ookstoreApplication.java
@@ -0,0 +1,11 @@
+package com.amorbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Amor3ookstoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Amor3ookstoreApplication.class, args);
+    }
+}
