02a2cf0d8273fa5ea3d1d7ae6d4fa7a13bb730bd
amor3ookstore <amor3ookstore@gmail.com>
2025-06-29 13:23:44 +0200

    Create CategoryDTO.java

---
+++ src/main/java/com/amorbookstore/dto/CategoryDTO.java
@@ -0,0 +1,13 @@
+package com.amorbookstore.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CategoryDTO {
+    private Long id;
+    private String name;
+}
