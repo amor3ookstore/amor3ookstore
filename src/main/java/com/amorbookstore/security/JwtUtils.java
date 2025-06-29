677197643ca184758df9c6de11266e29330ed8b3
amor3ookstore <amor3ookstore@gmail.com>
2025-06-29 13:38:50 +0200

    Create JwtUtils.java

---
+++ src/main/java/com/amorbookstore/security/JwtUtils.java
@@ -0,0 +1,44 @@
+package com.amorbookstore.security;
+
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+
+    @Value("${app.jwtSecret:SecretKeyJwt}")
+    private String jwtSecret;
+
+    @Value("${app.jwtExpirationMs:86400000}")
+    private int jwtExpirationMs;
+
+    public String generateToken(Authentication authentication) {
+        org.springframework.security.core.userdetails.User principal =
+                (org.springframework.security.core.userdetails.User) authentication.getPrincipal();
+
+        return Jwts.builder()
+                .setSubject(principal.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public String getUsernameFromJwtToken(String token) {
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        } catch (JwtException e) {
+            // log error if needed
+        }
+        return false;
+    }
+}
